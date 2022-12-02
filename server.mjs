14c09cc8455d1727150815acfaa621b815050c7e
Saaim-K <saaimkhan221@gmail.com>
2022-12-02 14:54:47 +0500

    React Express

--- server.mjs
+++ server.mjs
@@ -1,10 +1,18 @@
 import express from 'express'
+import path from 'path'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+const __dirname = path.resolve()
+
+app.use('/', express.static(
+    path.join(__dirname, './react-express/build')
+))
+
+
+app.get('/abc', (req, res) => {
     res.send('<h1>إِنَّ ٱلۡإِنسَٰنَ لَفِي خُسۡرٍ</h1>')
-    console.log('Request IP :',req.ip);
+    console.log('Request IP :', req.ip)
 })
 
 app.listen(port, () => {
