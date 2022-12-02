35d6c1228055903b28cdbc4c18b3e9380230dbcd
Saaim-K <saaimkhan221@gmail.com>
2022-12-02 21:10:15 +0500

    Succesful!!!

--- server.mjs
+++ server.mjs
@@ -5,15 +5,15 @@ const port = 3000
 
 const __dirname = path.resolve()
 
-app.use('/', express.static(
-    path.join(__dirname, './react-express/build')
-))
-
+app.use('/', express.static(path.join(__dirname, './react-express/build')))
 
 app.get('/abc', (req, res) => {
     res.send('<h1>إِنَّ ٱلۡإِنسَٰنَ لَفِي خُسۡرٍ</h1>')
     console.log('Request IP :', req.ip)
 })
+
+app.use('*', express.static(path.join(__dirname, './react-express/build')))
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
